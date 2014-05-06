a4f53d0780153d9dd7d10849d26c58e8f20bf7bc
antunesmg <antunesmg@hotmail.com>
2014-05-06 19:41:03 -0300

    inicio restricao

--- PAC_Laboratorio_Monitor.py
+++ PAC_Laboratorio_Monitor.py
@@ -1,8 +1,10 @@
 import Laboratorio
 import Monitores
+import Restricoes
 
 listalaboratorios = [] # lista com todos os laboratorios
 listamonitores = [] # lista com todos os monitores
+listarestricoes = [] # lista com todas as restriçoes
 loop = True
 
 f = open('entrada2.txt', 'r')
@@ -56,8 +58,32 @@ while loop == True:
                 if listalaboratorios[i].numero == int(lista[0]):
                         for j in range(1,len(lista)):
                                 listalaboratorios[i].adicionamonitor(lista[j])
-                        
-for i in range(0,len(listalaboratorios)):
-        print(listalaboratorios[i].numero)
-        print(listalaboratorios[i].dia)
-        print(listalaboratorios[i].horario)
+
+# le as restricoes
+loop = True
+while loop == True:
+        linha = f.readline()
+        if linha == '':
+                loop = False
+        else:
+#transforma "1={Marcos,Pedro}" em (1,Marcos,Pedro)
+                print(linha + 'l')
+                lista = linha.replace('.', ' ')
+                #lista = lista.replace('}', '')
+                #lista = lista.replace('=', ',')
+                #lista = lista.replace('\n', '')
+                lista = lista.split(' ')
+#enquanto a variavel(1) existir na listalaboratorios, acrescenta os monitores (Marcos,Pedro) ao dominio
+        if len(lista) == 3:
+                print('restricoes')
+                res = Restricoes.Restricoes(lista[0],lista[2],lista[1])
+                listarestricoes.append(res)
+        if len(lista) == 5:
+                res = Restricoes.Restricoes(lista[0],None,lista[2]) #nao precisa do ladodir, pois como o tipo é "p" já sabemos que devemos usar o dominio do ladoesq
+                listarestricoes.append(res)
+                
+for i in range(0,len(listarestricoes)):
+        print(listarestricoes[i].ladoesq)
+        print(listarestricoes[i].tipo)
+        print(listarestricoes[i].ladodir)
+        print(listarestricoes[i].estado)
