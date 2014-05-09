4f7cb1031cdb9a639abd99eac479449cb577bfc4
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-05-09 20:09:25 -0300

    conserto de bug  em __eq__

--- Variavel.py
+++ Variavel.py
@@ -10,7 +10,7 @@ class Variavel(object):
 
 	def __eq__(self, other):
 		selfAux = dict(self.__dict__)
-		otherAux = dict(self.__dict__)
+		otherAux = dict(other.__dict__)
 		del selfAux['dominio']
 		del otherAux['dominio']
 		return selfAux == otherAux
@@ -23,10 +23,10 @@ class Variavel(object):
 	def __str__(self):
 		aux = dict(self.__dict__)
 		del aux['dominio']
-		ret = ''
+		ret = '< '
 		for key, value in aux.iteritems():
 			ret += key + ' = ' + str(value) + ', '
-		return ret[:-2]
+		return ret[:-2] + '>'
 
 	def __repr__(self):
 		return self.__str__()
