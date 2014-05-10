a4ebe5f092a9dd773e052f3377e61ea8d37c4820
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-05-09 21:09:25 -0300

    prints para debug

--- testeGAC.py
+++ testeGAC.py
@@ -1,7 +1,7 @@
 from Restricao import *
 from ValorDominio import *
 from Variavel import *
-from GAC import *
+from GACDebug import *
 
 A = Variavel(1, **dict(nome='A'))
 A1 = ValorDominio(A, **dict(valor=1))
@@ -14,7 +14,7 @@ A.adicionaValor(A2)
 A.adicionaValor(A3)
 A.adicionaValor(A4)
 
-B = Variavel(1, **dict(nome='B'))
+B = Variavel(2, **dict(nome='B'))
 B1 = ValorDominio(B, **dict(valor=1))
 B2 = ValorDominio(B, **dict(valor=2))
 B3 = ValorDominio(B, **dict(valor=3))
@@ -30,8 +30,43 @@ def funcRestricaoAmenorB(valorDominioA, valorDominioB):
 
 restricaoAmenorB = Restricao([A, B], funcRestricaoAmenorB)
 
+print 'Teste do escopo e restricoes'
+print '' 
+print 'A pertenceEscopo de funcRestricaoAmenorB ? ' + str(restricaoAmenorB.pertenceEscopo(A))
+print 'B pertenceEscopo de funcRestricaoAmenorB ? ' + str(restricaoAmenorB.pertenceEscopo(B))
+
 TDA = geraTDA([A, B], [restricaoAmenorB])
+
+#def funcRestricaoAigual3(valorDominioA):
+#	return valorDominioA.valor == 3
+#
+#def funcRestricaoBigual2(valorDominioB):
+#	return valorDominioB.valor == 2
+#
+#restricaoAigual3 = Restricao([A], funcRestricaoAigual3)
+#restricaoBigual2 = Restricao([B], funcRestricaoBigual2)
+#
+#print 'Teste do escopo e restricoes'
+#print '' 
+#print 'A pertenceEscopo de funcRestricaoAigual3 ? ' + str(restricaoAigual3.pertenceEscopo(A))
+#print 'A pertenceEscopo de funcRestricaoBigual2 ? ' + str(restricaoBigual2.pertenceEscopo(A))
+#print 'B pertenceEscopo de funcRestricaoBigual2 ? ' + str(restricaoBigual2.pertenceEscopo(B))
+#print 'B pertenceEscopo de funcRestricaoAigual3 ? ' + str(restricaoAigual3.pertenceEscopo(B))
+#
+#TDA = geraTDA([A, B], [restricaoAigual3, restricaoBigual2])
+
+print ''
+print 'TDA:'
+print ''
+for arco in TDA:
+	print 'Arco=' + str(arco)
+
+print ''
+print 'GAC'
+print ''
 variaveisNovosDominios = GAC(TDA)
 
+print 'Dominios Finais'
 for var in variaveisNovosDominios:
+	print 'Dominio de ' + str(var)
 	print var.dominio
