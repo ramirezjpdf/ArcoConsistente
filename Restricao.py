54a67ffea2970b5eafc1a3c7d6e8accd460c4474
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-05-10 14:55:44 -0300

    escopo eh um set(conjunto)

--- Restricao.py
+++ Restricao.py
@@ -1,9 +1,9 @@
-from ValorDominio import *
+from ElementoDominio import *
 '''
 Classe que representa uma restricao
 
 propriedades: 
-->escopo eh uma lista de strings com as variaveis
+->escopo eh um set de variaveis(objetos da classe variavel)
 que fazem parte da restricao.
 -> funcaorestricao eh uma funcao que recebe
 como argumentos elementos do dominio das variaveis
@@ -14,6 +14,7 @@ satisfeita ou False caso contrario
 
 class Restricao:
 	def __init__(self, escopo, funcaorestricao):
+		if type(escopo) is not set: raise ValueError('argumento escopo deve ser set')
 		self.escopo = escopo
 		self.funcaorestricao = funcaorestricao
 
@@ -27,14 +28,14 @@ class Restricao:
 	return: lista de argumentos devidamente ordenados
 	'''
 	def _ordenaArgumentos(self, args):
-		return sorted(args, key=ValorDominio.getOrdem)
+		return sorted(args, key=ElementoDominio.getOrdem)
 
 	def chamafuncaorestricao(self, args):
 		args = self._ordenaArgumentos(args)
 		return self.funcaorestricao(*args)
 
 	def __eq__(self, other):
-		return self.funcaorestricao == other.funcaorestricao
+		return self.funcaorestricao == other.funcaorestricao and self.escopo == other.escopo
 
 	def __hash__(self):
 		return hash(str(self))
