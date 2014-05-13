72e92b375fd0ca9e18ff232494f7d603947d9af5
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-05-12 21:37:21 -0300

    parseia ate @RESTRICOES

--- parserAC.py
+++ parserAC.py
@@ -2,19 +2,49 @@ import re
 
 whitespacePattern        = re.compile(r'^[\s\n]+$')
 commentPattern           = re.compile(r'^//')
-variaveisPattern         = re.compile(r'^@VARIAVES')
+variaveisPattern         = re.compile(r'^@VARIAVEIS')
 atributosPattern         = re.compile(r'^%ATRIBUTOS')
 linhaAtributoPattern     = re.compile(r'([^,]+,)+')
-dominiosPattern         = re.compile(r'^@DOMINIOS')
+valoresAtributosPattern  = re.compile(r'^%%VALORES')
+dominiosPattern          = re.compile(r'^@DOMINIOS')
+restricoesPattern        = re.compile(r'@RESTRICOES')
 
-def parserArcoConsistencia(fo):
+def parseArcoConsistencia(fo):
 	atributosVariaveis = []
-	linhas = (l for l in fo if not commentPattern.match(l) and not whitespacePattern.match(l)): #remove as linhas em branco e comentarios
-	if not variaveisPattern.match(linhas.next()):
-		raise ValueError('ERRO!! @VARIAVEIS: secao nao encontrada ')
-	if not atributosPattern.match(linhas.next()):
-		raise ValueError('ERRO!! %ATRIBUTOS: subsecao nao encontrada')
 	valoresAtributosVariaveis = []
-	linha = ''
-	while not dominiosPattern.match((linha = linhas.next())):
-		atributosVariaveis.append(linha)
+
+	#remove as linhas em branco e comentarios
+	linhas = (l for l in fo if not commentPattern.match(l) and not whitespacePattern.match(l))
+	linha = linhas.next()
+	if not variaveisPattern.match(linha):
+		raise ValueError('ERRO!! @VARIAVEIS: secao nao encontrada ')
+	linha = linhas.next()
+	if not atributosPattern.match(linha):
+		raise ValueError('ERRO!! %ATRIBUTOS: subsecao nao encontrada')
+
+	linha = linhas.next()
+	while not valoresAtributosPattern.match(linha):
+		atributosVariaveis.append(linha[:-1])
+		linha = linhas.next()
+
+	linha = linhas.next()
+	while not dominiosPattern.match(linha):
+		valoresAtributosVariaveis.append(linha[:-1])
+		linha = linhas.next()
+
+	atributosDominios = []
+	valoresAtributosDominios = []
+	linha = linhas.next()
+	if not atributosPattern.match(linha):
+		raise ValueError('ERRO!! %ATRIBUTOS: subsecao nao encontrada')
+
+	linha = linhas.next()
+	while not valoresAtributosPattern.match(linha):
+		atributosDominios.append(linha[:-1])
+		linha = linhas.next()
+
+	linha = linhas.next()
+	while not restricoesPattern.match(linha):
+		valoresAtributosDominios.append(linha[:-1])
+		linha = linhas.next()
+	return atributosVariaveis, valoresAtributosVariaveis, atributosDominios, valoresAtributosDominios
